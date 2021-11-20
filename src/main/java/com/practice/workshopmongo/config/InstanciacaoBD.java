4b322b24d8f00b9ec5b44b7e3600022741f16955
thiago12prata <thiago12prata@gmail.com>
2021-11-20 18:01:42 -0300

    Referenciando os posts do usuario

--- src/main/java/com/practice/workshopmongo/config/InstanciacaoBD.java
+++ src/main/java/com/practice/workshopmongo/config/InstanciacaoBD.java
@@ -30,10 +30,15 @@ public class InstanciacaoBD implements CommandLineRunner{
 		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com");
 		Usuario u2= new Usuario(null, "Alex Green", "alex@gmail.com");
 		Usuario u3 = new Usuario(null, "Bob Grey", "bob@gmail.com");
+		
 		usuarioRepository.saveAll(Arrays.asList(u1,u2,u3));
 		
 		Post p1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Patiu Viagem","Vou para São paulo", new AutorDTO(u1));
 		Post p2 = new Post(null, Instant.parse("2018-03-23T19:53:07Z"), "Bom dia","Acordei feliz!", new AutorDTO(u1));		
+		
 		postRepository.saveAll(Arrays.asList(p1,p2));
+		
+		u1.getPosts().addAll(Arrays.asList(p1,p2));
+		usuarioRepository.save(u1);
 	}	
 }
--- src/main/java/com/practice/workshopmongo/entities/Usuario.java
+++ src/main/java/com/practice/workshopmongo/entities/Usuario.java
@@ -1,9 +1,12 @@
 package com.practice.workshopmongo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Usuario")
@@ -14,6 +17,9 @@ public class Usuario implements Serializable{
 	private String id;
 	private String nome;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public Usuario() {
 	}
@@ -40,6 +46,9 @@ public class Usuario implements Serializable{
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public List<Post> getPosts() {
+		return posts;
 	}
 	
 	@Override
